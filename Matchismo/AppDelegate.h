7d0ca5f5304d7a4b4cea3d7077cfaadde46b2638
henriquesb6 <henriquesb6@hotmail.com>
2015-01-07 15:12:01 -0200

    Initial Commit

---
+++ Matchismo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Matchismo
+//
+//  Created by Henrique Barros on 1/7/15.
+//  Copyright (c) 2015 Henrique Barros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Matchismo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Matchismo
+//
+//  Created by Henrique Barros on 1/7/15.
+//  Copyright (c) 2015 Henrique Barros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
